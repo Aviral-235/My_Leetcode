37e53086ff09c3c98626db6f81b06f4eb0dd9b9c
Aviral-235 <aviralgoswami23@gmail.com>
2026-01-25 14:05:49 +0530

    Time: 51 ms (16.72%) | Memory: 47.5 MB (62.09%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public static boolean check(List<Integer> list){
+        if(list.size()==2){
+            return true;
+        }
+        int diff= list.get(1)-list.get(0);
+        for(int i=2;i<list.size();i++){
+            if(list.get(i)-list.get(i-1)!=diff){
+                return false;
+            }
+        }
+        return true;
+    }
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> ans=new ArrayList<>();
+        for(int i=0;i<l.length;i++){
+            ArrayList<Integer> list=new ArrayList<>();
+            for(int j=l[i];j<=r[i];j++){
+                list.add(nums[j]);
+            }
+            Collections.sort(list);
+            if(check(list)==true){
+                ans.add(true);
+            }
+            else{
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+}
