ed9cf654bdca168b8de405b2d635f79b5623af1d
Aviral-235 <aviralgoswami23@gmail.com>
2026-01-27 19:30:29 +0530

    Time: 8 ms (39.18%) | Memory: 48.2 MB (7.84%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -1,18 +1,11 @@
 import java.util.*;
 class Solution {
     public boolean doesAliceWin(String s) {
-        Set<Character> set=new HashSet<>();
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
-                set.add(s.charAt(i));
-            }
+       for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
+            return true;
         }
-        count= set.size();
-        if(count==0){
-            return false;
-        }
-        
-        return true;
+       }
+        return false;
     }
 }
