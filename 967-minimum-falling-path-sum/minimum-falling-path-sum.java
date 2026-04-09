1a79abcb3adc0818360ae0b4fbb1f7da3e5bba4b
Aviral-235 <aviralgoswami23@gmail.com>
2026-04-09 13:22:47 +0530

    Time: 10 ms (7.22%) | Memory: 47.3 MB (54.53%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,29 +1,5 @@
 class Solution {
-    public static int path(int grid[][],int n,int m,int dp[][]){
-        if(m<0||m>grid[0].length-1){
-            return Integer.MAX_VALUE;
-        }
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-        else{
-        if(n==grid.length-1){
-            return grid[n][m];
-        }
-        return dp[n][m]=grid[n][m]+Math.min(path(grid,n+1,m,dp),Math.min(path(grid,n+1,m+1,dp),path(grid,n+1,m-1,dp)));
-        }
-    }
     public int minFallingPathSum(int[][] matrix) {
-        // int res=Integer.MAX_VALUE;
-        // int dp[][]=new int[matrix.length][matrix[0].length];
-        //     for(int edge[]:dp){
-        //         Arrays.fill(edge,-1);
-        //     }
-        // for(int i=0;i<matrix[0].length;i++){ 
-        //     int curr_res=path(matrix,0,i,dp);
-        //     res=Math.min(res,curr_res);
-        // }
-        // return res;
         int dp[][]=new int[matrix.length][matrix[0].length];
         for(int j=0;j<matrix[0].length;j++){
             dp[0][j]=matrix[0][j];
