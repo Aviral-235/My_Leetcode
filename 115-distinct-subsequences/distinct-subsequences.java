99071554cfce54d4ff8be7143b9a7b03c1591152
Aviral-235 <aviralgoswami23@gmail.com>
2026-05-20 10:53:46 +0530

    Time: 18 ms (76.98%) | Memory: 46.7 MB (86.35%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,36 +1,23 @@
 class Solution {
-    // int count(int n,int m,String s1,String s2,int dp[][]){
-    //     if(m<0){
-    //         return 1;
-    //     }
-    //      if(n<0){
-    //         return 0;
-    //      }
-    //      if(dp[n][m]!=-1){
-    //         return dp[n][m];
-    //      }
-    //      else{
-            
-    //      }
-    // }
     public int numDistinct(String s, String t) {
-        int dp[][]=new int[s.length()+1][t.length()+1];
-        for(int j=1;j<dp[0].length;j++){
-            dp[0][j]=0;
-        }
-        for(int i=0;i<dp.length;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[0].length;j++){
+        int prev[]=new int[t.length()+1];
+        
+        prev[0]=1;
+        
+        for(int i=1;i<s.length()+1;i++){
+            int curr[]=new int[t.length()+1];
+            curr[0]=1;
+            for(int j=1;j<prev.length;j++){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                    curr[j]=prev[j-1]+prev[j];
              }
              else{
-                 dp[i][j]=dp[i-1][j];
+                 curr[j]=prev[j];
              }
+             
             }
+            prev=curr;
         }
-        return dp[s.length()][t.length()];
+        return prev[t.length()];
     }
 }
