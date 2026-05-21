8590826ce60bf2c8d463d91971cd16e9ee9124c7
Aviral-235 <aviralgoswami23@gmail.com>
2026-05-21 09:41:44 +0530

    Time: 5 ms (69.00%) | Memory: 46.3 MB (94.38%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,22 +1,23 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int dp[][]=new int[word1.length()+1][word2.length()+1];
+        int prev[]=new int[word2.length()+1];
+        
         for(int i=0;i<=word2.length();i++){
-            dp[0][i]=i;
+            prev[i]=i;
         }
-        for(int i=0;i<=word1.length();i++){
-            dp[i][0]=i;
-        }
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[0].length;j++){
+        for(int i=1;i<word1.length()+1;i++){
+            int curr[]=new int[word2.length()+1];
+            curr[0]=i;
+            for(int j=1;j<word2.length()+1;j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
                 }
                 else{
-                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                    curr[j] = 1 + Math.min(curr[j-1], Math.min(prev[j], prev[j-1]));
                 }
             }
+            prev=curr;
         }
-        return dp[word1.length()][word2.length()];
+        return prev[word2.length()];
     }
 }
