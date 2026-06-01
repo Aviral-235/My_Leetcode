23dbe1a522a5411f30ea3468937b84d83046fafb
Aviral-235 <aviralgoswami23@gmail.com>
2026-06-01 10:01:31 +0530

    Time: 5 ms (96.09%) | Memory: 44.6 MB (51.02%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        long cost_=0;
+        int count=0;
+        for(int i=cost.length-1;i>=0;i--){
+            if(count==2){
+                count=0;
+                continue;
+            }
+            cost_+=cost[i];
+            count++;
+
+        }
+        return (int)cost_;
+    }
+}
