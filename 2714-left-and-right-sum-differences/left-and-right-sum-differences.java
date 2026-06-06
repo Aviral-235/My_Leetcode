77e941ec5398b097e99f81b51c48a9c04d1e960d
Aviral-235 <aviralgoswami23@gmail.com>
2026-06-06 08:46:19 +0530

    Time: 2 ms (98.84%) | Memory: 46.7 MB (13.15%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -1,23 +1,19 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
         int prefix[]=new int[nums.length];
-        prefix[0]=0;
-        int sum=0;
-        for(int i=1;i<nums.length;i++){
-            sum+=nums[i-1];
-            prefix[i]=sum;
-        }
-        sum=0;
         int suffix[]=new int[nums.length];
-        suffix[nums.length-1]=0;
-        for(int i=nums.length-2;i>=0;i--){
-            sum+=nums[i+1];
-            suffix[i]=sum;
+        int arr[]=new int[nums.length];
+        prefix[0]=0;
+        suffix[suffix.length-1]=0;
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+nums[i-1];
         }
-        int ans[]=new int[nums.length];
+        for(int i=suffix.length-2;i>=0;i--){
+            suffix[i]=suffix[i+1]+nums[i+1];
+        }
         for(int i=0;i<nums.length;i++){
-            ans[i]=Math.abs(prefix[i]-suffix[i]);
+            arr[i]=Math.abs(prefix[i]-suffix[i]);
         }
-        return ans;
+        return arr;
     }
 }
