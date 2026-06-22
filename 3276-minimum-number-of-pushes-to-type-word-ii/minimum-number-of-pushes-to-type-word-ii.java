3ce5fe35293a3ab26af178a3f66f844cf7195411
Aviral-235 <aviralgoswami23@gmail.com>
2026-06-22 19:35:36 +0530

    Time: 9 ms (94.51%) | Memory: 48.3 MB (7.17%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minimumPushes(String word) {
+        // Set<Character> set=new HashSet<>();
+        // for(int i=0;i<word.length();i++){
+        //     set.add(word.charAt(i));
+        // }
+        // if(set.size()<8){
+        //     return word.length();
+        // }
+        int arr[]=new int[26];
+        for(int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            arr[c-'a']++;
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length/2;i++){
+            int temp=arr[i];
+            arr[i]=arr[arr.length-i-1];
+            arr[arr.length-i-1]=temp;
+        }
+        int res=0;
+        int charCount=0;
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
+                charCount++;
+            }
+            int pushes= 0;
+            if(charCount%8==0){
+                pushes=charCount/8;
+            }
+            else{
+                pushes=(charCount/8)+1;
+            }
+            res+=arr[i]*pushes;
+        }
+        return res;
+    }
+}
