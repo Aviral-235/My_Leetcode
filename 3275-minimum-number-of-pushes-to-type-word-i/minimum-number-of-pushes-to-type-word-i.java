1e6ab9a649c83b4bbcfa472793a973481470df7d
Aviral-235 <aviralgoswami23@gmail.com>
2026-07-30 07:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (79.43%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumPushes(String word) {
+        if(word.length()<=8){
+            return word.length();
+        }
+        else if(word.length()<=16){
+            return 8+2*(word.length()-8);
+        }
+        else if(word.length()<=24){
+            return 24+3*(word.length()-16);
+        }
+        return 48+4*(word.length()-24);
+    }
+}
