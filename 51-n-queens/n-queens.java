240ac205370f08b292639afd849e6a3650323673
Aviral-235 <aviralgoswami23@gmail.com>
2026-09-16 22:35:16 +0530

    Time: 2 ms (81.75%) | Memory: 46.5 MB (63.99%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,54 +1,53 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> solutions = new ArrayList<>();
-        char[][] board = new char[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
+        List<List<String>> ans=new ArrayList<>();
+        char arr[][]=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]='.';
             }
         }
-
-        backtrack(0, board, solutions, n);
-        return solutions;
-    }
-
-    private void backtrack(int row, char[][] board, List<List<String>> solutions, int n) {
-        if (row == n) {
-            solutions.add(constructBoard(board));
-            return;
-        }
-
-        for (int col = 0; col < n; col++) {
-            if (isSafe(board, row, col, n)) {
-                board[row][col] = 'Q';
-                backtrack(row + 1, board, solutions, n);
-                board[row][col] = '.';
+        backtrack(0,arr,n,ans);
+        return ans;
             }
-        }
-    }
-
-    private boolean isSafe(char[][] board, int row, int col, int n) {
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q') return false;
-        }
-
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
-        }
-
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q') return false;
-        }
-
-        return true;
-    }
-
-    private List<String> constructBoard(char[][] board) {
-        List<String> result = new ArrayList<>();
-        for (char[] row : board) {
-            result.add(new String(row));
-        }
-        return result;
-    }
-}
+            public void backtrack(int row,char arr[][],int n,List<List<String>> ans){
+                if(row==n){
+                    ans.add(construct(arr));
+                    return;
+                }
+                for(int col=0;col<n;col++){
+                    if(solve(arr,row,col,n)){
+                        arr[row][col]='Q';
+                        backtrack(row+1,arr,n,ans);
+                        arr[row][col]='.';
+                    }
+                }
+            }    
+            public boolean solve(char arr[][],int row,int col,int n){
+                for(int i=0;i<row;i++){
+                    if(arr[i][col]=='Q'){
+                        return false;
+                    }
+                }
+                    for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+                        if(arr[i][j]=='Q'){
+                            return false;
+                        }
+                    }
+                    for(int i=row-1,j=col+1;i>=0&&j<n;i--,j++){
+                        if(arr[i][j]=='Q'){
+                            return false;
+                        }
+                    }
+                    
+                
+                return true;
+            }              
+            public List<String> construct(char arr[][]){
+                List<String> temp=new ArrayList<>();
+                for(char c[]:arr){
+                    temp.add(new String(c));
+                }
+                return temp;
+            }                                  
+}                           
